06e7e0e4ab12b453d27f93c9744b9147cba758df
Liodd <414838302@qq.com>
2017-04-09 19:33:50 +0800

    create basic xcproject and parse input

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Learning"
+    name: "Learning",
+    // add dependencies
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"),
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
+    ]
 )
+
+
